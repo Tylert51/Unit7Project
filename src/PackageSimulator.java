f898335578ebb6d6cc0848f33d7dd74eece38fe9
tyler <ttang7910@bths.edu>
2023-03-03 21:00:14 -0500

    got the random dimension generator working

--- src/PackageSimulator.java
+++ src/PackageSimulator.java
@@ -24,4 +24,12 @@ public class PackageSimulator {
     public void resetSimulation() {
         packages.clear();
     }
+
+    public double getRandDimension() {
+        return (Math.random() * 19.6) + 2;
+    }
+    public double getRandZipCode() {
+        return 0;
+
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -2,10 +2,16 @@ import java.util.ArrayList;
 
 public class Test {
     public static void main(String[] args) {
-        ArrayList<String> zipCodes = ZipCodeBuilder.getZipCodes("src/list_of_zip_codes");
-        System.out.println(zipCodes);
-        System.out.println(zipCodes.size());
 
+        int counter = 0;
+
+        for (int i = 0; i < 10000; i++) {
+            double rand = (Math.random() * 19.6) + 2;
+
+            if (rand > 21.5) {
+                System.out.println(rand);
+            }
+        }
 
     }
 }
